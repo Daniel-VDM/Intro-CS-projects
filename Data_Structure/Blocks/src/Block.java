9e3bd22e2c62b7bbf05644f459ce952512381829
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2018-01-07 01:50:20 -0800

    Fixed Indentation for Blocks src

--- Data_Structure/Blocks/src/Block.java
+++ Data_Structure/Blocks/src/Block.java
@@ -17,16 +17,16 @@ public class Block
    
    //Overloaded Constructors
    Block(int height, int width, int x, int y){
-	  this.blockCount++;
+     this.blockCount++;
       this.height = height;
       this.width = width;
       this.postition = Location.getLocationRef(x, y);
    }
    
    Block(int height, int width, Location position){
-	  this.blockCount++;
-	  this.height = height;
-	  this.width = width;
+     this.blockCount++;
+     this.height = height;
+     this.width = width;
       this.postition = position;
    }
    
@@ -39,12 +39,12 @@ public class Block
    
    
    /**
-	 * Generates an array of Location(s) which represent the spaces needed for 
-	 * a move in a given direction (the argument)
-	 * 
-	 * @param direction the Location direction desired
-	 * @return Location Array of free in desired direction
-	 */
+    * Generates an array of Location(s) which represent the spaces needed for 
+    * a move in a given direction (the argument)
+    * 
+    * @param direction the Location direction desired
+    * @return Location Array of free in desired direction
+    */
    public Location[] getFreeMoves(Location direction){
       int checkSize = 0;
       Location start = null, checkedDir = null;
@@ -75,19 +75,19 @@ public class Block
       //Generate the free spaces
       Location[] checked = new Location[checkSize];
       for(int i = 0; i < checkSize; i++)
-    	  checked[i] = start.add(checkedDir.mult(i));
+        checked[i] = start.add(checkedDir.mult(i));
       
       return checked;
       
    }
    
    /**
-	 * Checks if there is space for this block to move in the current board
-	 * 
-	 * @param free space needed for a move
-	 * @param emptyPlaces free spaces in current table
-	 * @return T/F if you can move the block
-	 */
+    * Checks if there is space for this block to move in the current board
+    * 
+    * @param free space needed for a move
+    * @param emptyPlaces free spaces in current table
+    * @return T/F if you can move the block
+    */
    public boolean movable(Location[] free, HashSet<Location> emptyLocation){
       if(free == null || free.length == 0)
          return false;
@@ -98,40 +98,40 @@ public class Block
    }
    
    /**
-	 * Move current block in the direction desired
-	 * 
-	 * @param direction direction that is desired
-	 * @param occupied Array of occupied Location places
-	 * @param empty HashSet of empty Location places
-	 */
+    * Move current block in the direction desired
+    * 
+    * @param direction direction that is desired
+    * @param occupied Array of occupied Location places
+    * @param empty HashSet of empty Location places
+    */
    public void move(Location direction, Location[] occupied, 
-		   								HashSet<Location> empty){
+                                 HashSet<Location> empty){
       Location newFreeTuple = null;
       int stepsToOppSide = (occupied.length == this.width) ? this.height 
-				 											:this.width;
+                                             :this.width;
       this.postition = this.postition.add(direction);
       for(Location p: occupied){
-    	  empty.remove(p);
+        empty.remove(p);
          if(direction == Location.NORTH || direction == Location.WEST)
-        	 newFreeTuple = p.add(direction.mult(-stepsToOppSide));
+          newFreeTuple = p.add(direction.mult(-stepsToOppSide));
          if(direction == Location.SOUTH || direction == Location.EAST)
-        	 newFreeTuple = p.sub(direction.mult(stepsToOppSide));
+          newFreeTuple = p.sub(direction.mult(stepsToOppSide));
          empty.add(newFreeTuple);
       }      
    }
    
    //Assumes that argument is always a block object
    public boolean equals(Object o){
-		  Block other = (Block) o;
-	      return (other.postition == this.postition 
-	    		  && other.height == this.height 
-	    		  && other.width == this.width);
-	   }
-	   
+        Block other = (Block) o;
+         return (other.postition == this.postition 
+              && other.height == this.height 
+              && other.width == this.width);
+      }
+      
    public int hashCode(){
       return (int) (this.height*(Math.pow(this.blockCount, 3)) 
-    		  + this.width*(Math.pow(this.blockCount, 2))
-    		  + this.getRowPosition() * this.blockCount + this.getColPosition());
+           + this.width*(Math.pow(this.blockCount, 2))
+           + this.getRowPosition() * this.blockCount + this.getColPosition());
    }
    
    public String toString(){
@@ -148,44 +148,44 @@ public class Block
    
    //Test Main for this Class
    public static void main(String[] args){
-		Location.genBoardLocations(3, 3);
-		Block b;
-		HashSet<Location> free;
-		
-		//Testing movement on a 2x2 block in the bottom right corner
-		Location[] move;
-		for (Location dir: Location.DIRECTIONS) {
-			free = new HashSet<>();
-			b = new Block(2,2,1,1);
-			free.add(Location.getLocationRef(0, 0));
-			free.add(Location.getLocationRef(1, 0));
-			free.add(Location.getLocationRef(2, 0));
-			free.add(Location.getLocationRef(0, 1));
-			free.add(Location.getLocationRef(0, 2));
-			
-			move = b.getFreeMoves(dir);
-			System.out.println("Block: " + b);
-		System.out.println("Free Spaces: " + free);
-		System.out.println("Direction: " + dir);
-		
-		if(b.movable(move, free)) {
-			System.out.println("Moved");
-			b.move(dir, move, free);
-			System.out.println("Moved Block:" + b + "\n");
-		} else
-			System.out.println("No Move\n");
-		}
-		
-		System.out.println("Testing equals");
-		Block b1 = new Block(2, 2, 1, 1);
-		Block b2 = new Block(2, 2, 1, 1);
-		Block b3 = new Block(2, 2, 0, 1);
-		System.out.println(b1 + " == " + b2 + " -> " + b1.equals(b2));
-		System.out.println(b1 + " == " + b3 + " -> " + b1.equals(b3));
-		
-		System.out.println("\nTesting Copy:\n Copy: " + b3 + " --> " + b3.copy());
-		
-		System.out.println("\nTesting hashCode of " + b1 + ": " + b1.hashCode());
+      Location.genBoardLocations(3, 3);
+      Block b;
+      HashSet<Location> free;
+      
+      //Testing movement on a 2x2 block in the bottom right corner
+      Location[] move;
+      for (Location dir: Location.DIRECTIONS) {
+         free = new HashSet<>();
+         b = new Block(2,2,1,1);
+         free.add(Location.getLocationRef(0, 0));
+         free.add(Location.getLocationRef(1, 0));
+         free.add(Location.getLocationRef(2, 0));
+         free.add(Location.getLocationRef(0, 1));
+         free.add(Location.getLocationRef(0, 2));
+         
+         move = b.getFreeMoves(dir);
+         System.out.println("Block: " + b);
+      System.out.println("Free Spaces: " + free);
+      System.out.println("Direction: " + dir);
+      
+      if(b.movable(move, free)) {
+         System.out.println("Moved");
+         b.move(dir, move, free);
+         System.out.println("Moved Block:" + b + "\n");
+      } else
+         System.out.println("No Move\n");
+      }
+      
+      System.out.println("Testing equals");
+      Block b1 = new Block(2, 2, 1, 1);
+      Block b2 = new Block(2, 2, 1, 1);
+      Block b3 = new Block(2, 2, 0, 1);
+      System.out.println(b1 + " == " + b2 + " -> " + b1.equals(b2));
+      System.out.println(b1 + " == " + b3 + " -> " + b1.equals(b3));
+      
+      System.out.println("\nTesting Copy:\n Copy: " + b3 + " --> " + b3.copy());
+      
+      System.out.println("\nTesting hashCode of " + b1 + ": " + b1.hashCode());
 
    }
 }
